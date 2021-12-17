9c4732fc67196721e2d5ca2abba7011f3da19c07
Michael Lynn <merlynn@gmail.com>
2021-12-17 10:33:59 -0500

    Fix logout

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,4 +1,6 @@
-import User from '../models/user'
+import User from '../models/user';
+import jsonwebtoken from 'jsonwebtoken';
+require("dotenv").config();
 
 export const register = async (req, res) => {
     console.log(req.body);
@@ -15,5 +17,39 @@ export const register = async (req, res) => {
     } catch(err) {
         console.log('Create user failed.');
         return res.status(400).send('Error: User creation failed');
+    }
+}
+
+export const login = async (req, res) => {
+    console.log(req.body);
+    const { email, password } = req.body;
+    if (!email) return res.status(400).send('Email is required');
+    if (!password || password.length < 6) return res.status(400).send('Password is required and should be min 6 characters');
+    try {
+        let user = await User.findOne({email}).exec();
+        if (!user) res.status(400).send("User with that email not found.");
+        console.log("User Exists", user);
+        console.log("Login successful.");
+        user.comparePassword(password, (err, match) => {
+            console.log("COMPARE PASSWORD IN LOGIN ERROR", err);
+            if (!match || err) {
+                return res.status(400).send("Wrong password");
+            }
+            console.log("GENERATE A TOKEN THEN SEND AS RESPONSE TO CLIENT");
+            let token = jsonwebtoken.sign({_id: user._id}, process.env.JWT_SECRET, {
+                expiresIn: '7d'
+            });
+            res.json({token, user: {
+                name: user.name,
+                email: user.email,
+                _id: user._id,
+                createdAt: user.createdAt,
+                updatedAt: user.updatedAt
+            }});
+        })
+        
+    } catch(err) {
+        console.log('Login user failed.');
+        return res.status(400).send('Error: User login failed');
     }
 }
